c7f52720d463981092da3f7d83d469cbcc218378
Md.Jahid Hasan <jahidhasan5803733@gmail.com>
2025-11-19 12:12:35 +0600

    __destruct()-done

--- oop.php
+++ oop.php
@@ -2,22 +2,38 @@
 
 <?php
 
-class Car{
-    public $name;
-    public $color;
-    public $Price;
+// class Car{
+//     public $name;
+//     public $color;
+//     public $Price;
 
-    function __construct($name,$color)
-    {
-        $this->name=$name;
-        $this->color=$color;
-    }
+//     function __construct($name,$color)
+//     {
+//         $this->name=$name;
+//         $this->color=$color;
+//     }
+// }
+
+// $Car1=new Car('Marceds','Red');
+// // $Car1->get_name('Marcedes','Red');
+// echo $Car1->color;
+
+// echo $Car1->name;
+
+class Fruit {
+  // Properties
+  var $name;
+  var $color;
+
+  // Methods
+  function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color; 
+  }
+  function __destruct() {
+    echo "The fruit is {$this->name} and the color is {$this->color}."; 
+  }
 }
 
-$Car1=new Car('Marceds','Red');
-// $Car1->get_name('Marcedes','Red');
-echo $Car1->color;
-
-echo $Car1->name;
-
+$apple = new Fruit("Apple", "red");
 ?>
